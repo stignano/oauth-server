766514848b49db7e375e1ab9f4f4f5789955aae6
Tom Ierino <19969307@student.curtin.edu.au>
2020-08-02 15:25:23 +0800

    google strategy redirect settings

--- services/passport.js
+++ services/passport.js
@@ -25,6 +25,7 @@ passport.use(
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback",
+      proxy: true,
     },
     (accessToken, refreshToken, profile, done) => {
       // User.findOne returns a promise, so use 'then' here
